af6da7aa6057aca0918a717db41ad2cfdd5a42c0
portilloronny6 <portilloronny6@gmail.com>
2021-08-21 19:36:49 -0400

    numberguesser

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# numberguesser-js
+# Hi, I'm Ronny! 👋
+
+## 👨🏾‍💻 About Me
+
+I'm Python and JavaScript Developer... Although my native programming language is Python 🐍.
+
+## 🛠 Skills
+
+Python, Javascript, HTML, CSS, Git, Unit Testing, Asynchronous programming, Django, API RESTFul, Django Rest FrameWork, Machine learning, Deep Learning, etc.
+
+# Project
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://en.wikipedia.org/wiki/MIT_License)
+
+# Task List | JavaScript
+
+This project has been created by **Ronny Portillo** in order to be able to guess a number between 1 and 10.
+
+To test the project please go to the following link: <a href="https://portilloronny6.github.io/loancalculator-js/" target="_blank">Project</a>
+
+![Logo](Screenshot_1.png)
+
+## Tech using in this project
+
+**Language:** HTML, CSS, JavaScript
+
+**Server:** Github
+
+## Support
+
+For support, email portilloronny6@gmail.com or visit <a href="https://portilloronny6.github.io/website/" target="_blank">my personal website</a>.
---
+++ app.js
@@ -0,0 +1,80 @@
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+// Games values
+let min = 1,
+  max = 10,
+  winningNumber = getRandomNum(max, min),
+  guessesLeft = 3;
+
+//   UI Elements
+const game = document.querySelector("#game"),
+  minNumber = document.querySelector(".min-num"),
+  maxNumber = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+// Assign UI min and max
+minNumber.textContent = min;
+maxNumber.textContent = max;
+
+// Play again and reload
+
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
+// listen for guess
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  //   Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please, enter a number between ${min} and ${max}`, "red");
+    return;
+  }
+
+  //   Check if won
+
+  if (guess === winningNumber) {
+    gameOver(true, `${winningNumber} is correct🥳! YOU WON!`);
+  } else {
+    guessesLeft -= 1;
+    setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, "red");
+    guessInput.value = "";
+    if (guessesLeft === 0) {
+      gameOver(false, `Game over. You lost 😭 correct name was ${guess}`);
+      guessInput.remove();
+    }
+  }
+});
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg, color);
+
+  guessBtn.value = "Play again";
+  guessBtn.style.borderColor = "black";
+  guessBtn.className += "play-again";
+}
+
+function getRandomNum(max, min) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
